69e3ba6ef8db96d04a9d2583108cd8289d2ae543
dbetebenner <dbetebenner@gmail.com>
2015-02-06 13:34:40 -0500

    Update to 0.0-8, chaning embedded data to environment, citation, ...

--- R/randomNames.R
+++ R/randomNames.R
@@ -8,17 +8,16 @@ function(
 	name.sep=", ") {
 
 	.N <- V1 <- NULL ## To prevent R CMD check warnings
-	randomNamesData <- randomNamesData
                         
 	first_names <- function(tmp.gender, tmp.ethnicity, tmp.number) {
 		tmp.gender <- tmp.gender[1]; tmp.ethnicity <- tmp.ethnicity[1]
-		tmp <- randomNamesData[[paste("first_names_e", tmp.ethnicity, "_g", tmp.gender, sep="")]]
+		tmp <- randomNames::randomNamesData[[paste("first_names_e", tmp.ethnicity, "_g", tmp.gender, sep="")]]
 		suppressWarnings(sample(rownames(tmp), tmp.number, replace=TRUE, prob=tmp))
 	} 
 
 	last_names <- function(tmp.ethnicity, tmp.number) {
 		tmp.ethnicity <- tmp.ethnicity[1]
-		tmp <- randomNamesData[[paste("last_names_e", tmp.ethnicity, sep="")]]
+		tmp <- randomNames::randomNamesData[[paste("last_names_e", tmp.ethnicity, sep="")]]
 		suppressWarnings(sample(rownames(tmp), tmp.number, replace=TRUE, prob=tmp))
 	}
 
